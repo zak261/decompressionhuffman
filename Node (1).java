2dc66e4f90415d5814b769034bdc2d7e49f23661
zak261 <154206681+zak261@users.noreply.github.com>
2024-05-02 23:17:33 +0200

    Update Node (1).java

--- Node (1).java
+++ Node (1).java
@@ -1,5 +1,7 @@
 // Node.java
-public class Node {
+public class Node {// stocker la structure de l'arbre de Huffman. 
+    	//Un noeud contient une valeur (qui peut être un caractère ou une fréquence), 
+    	//et des liens vers les noeuds enfants gauche et droit.
     public char value;
     public Node left, right;
 
